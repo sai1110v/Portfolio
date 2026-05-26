6f37a463242d6bce42e49a944911b338167b3829
Sure Shraavan <saivegisetti46@gmail.com>
2026-05-26 22:31:17 +0530

    update techboard

--- src/components/TechBoard.jsx
+++ src/components/TechBoard.jsx
@@ -26,90 +26,25 @@ import {
 } from "react-icons/si";
 
 const techIcons = [
-  {
-    icon: <FaReact />,
-    color: "text-cyan-400",
-  },
+  { icon: <FaReact />, color: "text-cyan-400" },
+  { icon: <SiJavascript />, color: "text-yellow-400" },
+  { icon: <FaNodeJs />, color: "text-green-500" },
+  { icon: <SiMongodb />, color: "text-green-400" },
 
-  {
-    icon: <SiJavascript />,
-    color: "text-yellow-400",
-  },
+  { icon: <FaPython />, color: "text-blue-400" },
+  { icon: <FaHtml5 />, color: "text-orange-500" },
+  { icon: <FaCss3Alt />, color: "text-blue-500" },
+  { icon: <FaGitAlt />, color: "text-orange-400" },
 
-  {
-    icon: <FaNodeJs />,
-    color: "text-green-500",
-  },
+  { icon: <FaGithub />, color: "text-white" },
+  { icon: <SiTailwindcss />, color: "text-cyan-300" },
+  { icon: <SiExpress />, color: "text-gray-300" },
+  { icon: <SiMysql />, color: "text-blue-300" },
 
-  {
-    icon: <SiMongodb />,
-    color: "text-green-400",
-  },
-
-  {
-    icon: <FaPython />,
-    color: "text-blue-400",
-  },
-
-  {
-    icon: <FaHtml5 />,
-    color: "text-orange-500",
-  },
-
-  {
-    icon: <FaCss3Alt />,
-    color: "text-blue-500",
-  },
-
-  {
-    icon: <FaGitAlt />,
-    color: "text-orange-400",
-  },
-
-  {
-    icon: <FaGithub />,
-    color: "text-white",
-  },
-
-  {
-    icon: <SiTailwindcss />,
-    color: "text-cyan-300",
-  },
-
-  {
-    icon: <SiExpress />,
-    color: "text-gray-300",
-  },
-
-  {
-    icon: <SiMysql />,
-    color: "text-blue-300",
-  },
-
-  {
-    icon: <FaJava />,
-    color: "text-red-400",
-  },
-
-  {
-    icon: <SiC />,
-    color: "text-blue-400",
-  },
-
-  {
-    icon: <SiFirebase />,
-    color: "text-yellow-500",
-  },
-
-  {
-    icon: <FaFigma />,
-    color: "text-pink-400",
-  },
-
-  {
-    icon: <SiPostman />,
-    color: "text-orange-500",
-  },
+  { icon: <FaJava />, color: "text-red-400" },
+  { icon: <SiC />, color: "text-blue-400" },
+  { icon: <SiFirebase />, color: "text-yellow-500" },
+  { icon: <SiPostman />, color: "text-orange-500" },
 ];
 
 const TechBoard = () => {
@@ -119,7 +54,7 @@ const TechBoard = () => {
     <motion.div
 
       animate={{
-        y: [0, -15, 0],
+        y: [0, -10, 0],
       }}
 
       transition={{
@@ -128,32 +63,32 @@ const TechBoard = () => {
         ease: "easeInOut",
       }}
 
-      className="relative scale-90"
+      className="relative scale-35"
     >
 
       <Tilt
 
-        tiltMaxAngleX={15}
-        tiltMaxAngleY={15}
+        tiltMaxAngleX={12}
+        tiltMaxAngleY={12}
 
-        perspective={1200}
+        perspective={1000}
 
-        transitionSpeed={2000}
+        transitionSpeed={1500}
 
-        scale={1.02}
+        scale={1.01}
 
         gyroscope={true}
 
         className="
-        bg-[#0b0b0b]
-        rounded-[30px]
-        p-1
-        border border-red-500/10
-        shadow-[0_0_60px_rgba(255,0,0,0.12)]
-      "
+          bg-[#0b0b0b]
+          rounded-[28px]
+          p-1
+          border border-red-500/10
+          shadow-[0_0_40px_rgba(255,0,0,0.10)]
+        "
       >
 
-        <div className="grid grid-cols-4 gap-4">
+        <div className="grid grid-cols-4 gap-3">
 
           {techIcons.map((item, index) => (
 
@@ -162,25 +97,24 @@ const TechBoard = () => {
               key={index}
 
               whileHover={{
-                scale: 1.12,
-                rotate: 5,
+                scale: 1.08,
               }}
 
               className="
-              w-5
-              h-5
-              bg-[#111]
-              rounded-2xl
-              flex
-              items-center
-              justify-center
-              shadow-lg
-              border border-white/5
-            "
+                w-5
+                h-5
+                bg-[#111]
+                rounded-xl
+                flex
+                items-center
+                justify-center
+                shadow-lg
+                border border-white/5
+              "
             >
 
               <div
-                className={`text-3xl ${item.color}`}
+                className={`text-2xl ${item.color}`}
               >
                 {item.icon}
               </div>
